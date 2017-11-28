bcb5b92396885637f6dc4c54c46fd5f2d1056581
Atsushi Shimono <atsushi@himor.in>
2017-11-28 10:17:03 +0900

    Update to print VCPU count

--- libvirt/nodeinfo.py
+++ libvirt/nodeinfo.py
@@ -9,36 +9,38 @@ DEF_GB_MB = 1024.0
 DEF_GB_KB = 1048576.0
 DEF_GB_B  = 1073741824.0
 
+ERR_NOT_RUNNING = 1
+ERR_NO_CONNECT = 2
+
 def print_server(name, target_uri):
     try:
         lv_conn = libvirt.openReadOnly(target_uri)
     except libvirt.libvirtError:
-        print "Target '{}' ({}) is not running".format(name, target_uri)
-        return None
+        return ERR_NOT_RUNNING
     if lv_conn == None:
-        print "Failed to connect '{}'".format(target_uri)
-        return None
-    print "Information for '%s':" % (name)
+        return ERR_NO_CONNECT
+    print("Information for '{0:s}':".format(name))
     node_info = lv_conn.getInfo()
     node_mem = lv_conn.getMemoryStats(libvirt.VIR_NODE_MEMORY_STATS_ALL_CELLS)
-    print ("  Memory : {:.1f} GB / {:.1f} GB free, {:.1f} GB cached, " + \
+    print(("  Memory : {:.1f} GB / {:.1f} GB free, {:.1f} GB cached, " + \
         "{:.1f} GB buffers").format( \
         node_mem['total'] / DEF_GB_KB, node_mem['free'] / DEF_GB_KB, 
-        node_mem['cached'] / DEF_GB_KB, node_mem['buffers'] / DEF_GB_KB)
-    print ("  VCPUs  : {0[0]:d} in {0[1]:d} MHz ({0[2]:d} NUMA, " + \
+        node_mem['cached'] / DEF_GB_KB, node_mem['buffers'] / DEF_GB_KB))
+    print(("  VCPUs  : {0[0]:d} in {0[1]:d} MHz ({0[2]:d} NUMA, " + \
         "{0[3]:d} socket, {0[4]:d} core, {0[5]:d} thread)").format( \
-        node_info[2:8])
+        node_info[2:8]))
     # getCPUStats(cpu_id) seems to return in cumulative nsec
-    print "  CPU    : Model \"{}\"".format(node_info[0])
+    print("  CPU    : Model \"{}\"".format(node_info[0]))
     if node_info[4] > 1:
-        print "  Memory per NUMA"
+        print("  Memory per NUMA")
         for numa in xrange(node_info[4]):
             buf = lv_conn.getMemoryStats(numa)
-            print "    NUMA {:d}: {:.1f} GB free in {:.1f} GB total".format( \
-                numa, buf['free'] / DEF_GB_KB, buf['total'] / DEF_GB_KB)
+            print("    NUMA {:d}: {:.1f} GB free in {:.1f} GB total".format( \
+                numa, buf['free'] / DEF_GB_KB, buf['total'] / DEF_GB_KB))
     lv_doms = lv_conn.listAllDomains(0)
-    print "  domains:"
+    print("  domains:")
     mem_doms = [0, 0] # [max, cur]
+    cpu_doms = [0, 0] # [max, cur]
     for lv_proc in lv_doms:
         lv_info = lv_proc.info()
         lv_state = _getStringForState(lv_info[0])
@@ -47,18 +49,24 @@ def print_server(name, target_uri):
         mem_doms[0] += lv_info[1]
         mem_doms[1] += lv_info[2]
         if lv_info[0] == libvirt.VIR_DOMAIN_RUNNING:
-            print ("    {0:15s} ({1:2d}): {2:s} Mem {3[2]:.1f}GB " + \
-                "({3[1]:.1f}GB max) {3[3]:d}CPU ({4:d} max)") \
-                .format(lv_proc.name(), lv_proc.ID(), lv_state, lv_info, \
-                lv_proc.maxVcpus())
+            print(("    {0:15s} ({1:2d}): {2:s} Mem {3[2]:.1f}GB " + \
+                "({3[1]:.1f}GB max) {3[3]:d}CPU ({4:d} max)" \
+                ).format(lv_proc.name(), lv_proc.ID(), lv_state, lv_info, \
+                lv_proc.maxVcpus()))
+            cpu_doms[0] += lv_proc.maxVcpus()
+            cpu_doms[1] += lv_info[3]
         else:
-            print ("    {0:15s} (--): {1:s} Mem {2[2]:.1f}GB " + \
-                "({2[1]:.1f}GB max) -CPU ({2[3]:d} max)") \
-                .format(lv_proc.name(), lv_state, lv_info)
+            print(("    {0:15s} (--): {1:s} Mem {2[2]:.1f}GB " + \
+                "({2[1]:.1f}GB max) -CPU ({2[3]:d} max)" \
+                ).format(lv_proc.name(), lv_state, lv_info))
+            cpu_doms[0] += lv_info[3]
     mem_free = node_info[1] / DEF_GB_MB - mem_doms[0]
-    print ("  Memory usage: {:.1f}GB ({:.1f}GB max) {:.1f}GB allocatable"
-        ).format(mem_doms[1], mem_doms[0], mem_free)
+    print("  Memory usage: {:.1f}GB ({:.1f}GB max) {:.1f}GB allocatable" \
+        .format(mem_doms[1], mem_doms[0], mem_free))
+    print("  CPU usage   : {:d} ({:d} total) {:d} VCPUs allocatable" \
+        .format(cpu_doms[1], cpu_doms[0], (node_info[2] - cpu_doms[0])))
     lv_conn.close()
+    return None
 
 def _getStringForState(state):
     if state == libvirt.VIR_DOMAIN_NOSTATE:
@@ -86,14 +94,32 @@ def main(cmd_opt):
     try:
         objConfig.Load(cmd_opt)
     except e:
-        print "Configuration '%s' load error: %s" % (cmd_opt, e)
+        print("Configuration '{:s}' load error: {:s}".format(cmd_opt, e))
+    not_run = []
+    no_conn = []
     for item in objConfig.GetListServers():
-        print_server(item, objConfig.GetUri(item))
-        print ""
+        err = print_server(item, objConfig.GetUri(item))
+        if err == ERR_NOT_RUNNING:
+            not_run.append(item)
+        elif err == ERR_NO_CONNECT:
+            no_conn.append(item)
+        else:
+            print("")
+    print("")
+    if len(not_run) > 0:
+        print("Targets not running: ")
+        for item in not_run:
+            print(" {:s}".format(item))
+        print("")
+    if len(no_conn) > 0:
+        print("Targets failed to connect: ")
+        for item in no_conn:
+            print(" {:s}".format(item))
+        print("")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print "Usage: <script> <json configuration file>"
+        print("Usage: <script> <json configuration file>")
         sys.exit(1)
     main(sys.argv[1])
 
